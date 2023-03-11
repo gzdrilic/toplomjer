12c48c2258c4214aa7576c3ae83d1e3c78b535bb
Dominik <dominikgaspar@windowslive.com>
2023-03-11 16:00:38 +0100

    /doctor-dashboard + /showPatientRecords mappings added

---
+++ src/main/java/com/toplomjer/toplomjer/DoctorController.java
@@ -0,0 +1,49 @@
+package com.toplomjer.toplomjer;
+
+import com.toplomjer.toplomjer.model.Record;
+import com.toplomjer.toplomjer.model.RecordRepository;
+import com.toplomjer.toplomjer.model.User;
+import com.toplomjer.toplomjer.model.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class DoctorController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RecordRepository recordRepository;
+
+    User currUser;
+
+
+    //Ovdje pretpostavljam da ce svi doktori imati ovlasti vidjeti sve pacijente i njihove zapise
+
+    @GetMapping("/doctor-dashboard")
+    public String showDashboard(Model model, long id) {
+        currUser = userRepository.findById(id);
+        List<User> patientList = userRepository.findByPermissionlevel(1);
+        model.addAttribute("currUser", currUser);
+        model.addAttribute("patientList", patientList);
+        return "doctor-dashboard.html";
+    }
+
+
+    @GetMapping("/showPatientRecords")
+    public String showPatientRecords(Model model, long id) {
+        User currSelectedPatient = userRepository.findById(id);
+        List<Record> recordsList = recordRepository.findByPatient(currSelectedPatient);
+        model.addAttribute("currUser", currUser);
+        model.addAttribute("currPatient", currSelectedPatient);
+        model.addAttribute("recordsList", recordsList);
+        return "patient-records.html";
+    }
+
+
+}
