2a71f4975cc5f83aa6f791e5ce535853cb799958
Dominik <dominikgaspar@windowslive.com>
2023-03-23 13:54:59 +0100

    Added adress + phone number for user.

--- src/main/java/com/toplomjer/toplomjer/model/User.java
+++ src/main/java/com/toplomjer/toplomjer/model/User.java
@@ -25,17 +25,27 @@ public class User {
 
     private String password;
 
+    private String email;
+
+    private String phoneNumber;
+
+    private String address;
+
 
     public User() {
     }
 
-    public User(String firstName, String lastName, String username, String password, int permissionLevel) {
+    public User(String firstName, String lastName, String username, String password, String address, String phoneNumber,  int permissionLevel) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.username = username;
         this.password = password;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
         this.permissionLevel = permissionLevel;
     }
+
+
 
 
     public String getFirstName() {
@@ -86,6 +96,30 @@ public class User {
         this.id = id;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -95,6 +129,9 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 ", username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
                 '}';
     }
 }
